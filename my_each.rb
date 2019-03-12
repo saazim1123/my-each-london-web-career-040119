da4a09f9f9a2102e99a5b3a5e4eeacb4d881bb3e
Adnan Azim <saazim1123@gmail.com>
2019-03-12 19:11:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each (array)
   # code here
+  i = 0 
+  while i < array
+  yield(array[i])
+  i += 1
+  end
+  array
+end
+
+my_each(collection) do
+  i
 end
